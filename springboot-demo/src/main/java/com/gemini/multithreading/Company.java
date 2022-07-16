d89ad8ea61aba49c8e67c949b56b6ff63eaf3016
Gem-Jasvinder <jasvinder.singh@geminisolutions.com>
2022-07-16 12:15:17 +0530

    Multithreading:v2

--- springboot-demo/src/main/java/com/gemini/multithreading/Company.java
+++ springboot-demo/src/main/java/com/gemini/multithreading/Company.java
@@ -4,40 +4,53 @@ import com.gemini.imageUploading.service.StudentServicesImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
+import java.util.concurrent.*;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 @Component
 public class Company {
 
-    Queue<Integer> queue=new LinkedList<>();
-    int size=10;
+    BlockingQueue<Integer> queue=new LinkedBlockingQueue<>(10);
+//    int size=10;
     int n;
     public static final Logger logger= LoggerFactory.getLogger(Company.class);
-    synchronized public void produceItem(int n){
-        while(queue.size()==10){
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                logger.info("Interrupted Exception");
-            }
-        }
-        queue.add(this.n=n);
-        logger.info("Produced:"+Thread.currentThread().getId()+" "+n);
-        notify();
+     public void produceItem(int n){
+//        while(queue.size()==size){
+//            try {
+////                wait();
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                logger.info("Interrupted Exception");
+//            }
+//        }
+         try {
+             queue.put(this.n=n);
+         } catch (InterruptedException e) {
+             logger.info("Runtime Exception");
+         }
+         logger.info("Produced:"+Thread.currentThread().getId()+" "+n);
+       // notify();
     }
 
-    synchronized public int consumeItem (){
-        while(queue.size()==0){
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                logger.info("Interrupted Exception");
-            }
-        }
-        logger.info("Consumed:"+Thread.currentThread().getId()+" "+queue.remove());
-        notify();
+     public int consumeItem (){
+//        while(queue.size()==0){
+//            try {
+////                wait();
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                logger.info("Interrupted Exception");
+//            }
+//        }
+         try {
+             logger.info("Consumed:"+Thread.currentThread().getId()+" "+queue.take());
+         } catch (InterruptedException e) {
+             logger.info("Runtime Exception");
+         }
+         // notify();
         return n;
     }
 }
